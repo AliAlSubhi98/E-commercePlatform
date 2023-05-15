170406981e042da92c54934f916f2138acd2ea4a
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 11:03:12 +0400

    RatingService

---
+++ src/main/java/com/alialsubhi/demoProject1/Services/RatingService.java
@@ -0,0 +1,19 @@
+package com.alialsubhi.demoProject1.Services;
+
+import com.alialsubhi.demoProject1.Models.Rating;
+import com.alialsubhi.demoProject1.Repositories.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RatingService {
+
+    @Autowired
+    RatingRepository ratingRepository;
+
+    List<Rating> getAllRatings(){
+        return ratingRepository.findAll();
+    }
+}
