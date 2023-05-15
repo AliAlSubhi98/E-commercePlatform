29ee2e24eb061598ecee200db8461def5a03de70
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 10:04:31 +0400

    create class Order

--- src/main/java/com/alialsubhi/demoProject1/Models/Order.java
+++ src/main/java/com/alialsubhi/demoProject1/Models/Order.java
@@ -1,4 +1,29 @@
 package com.alialsubhi.demoProject1.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Setter
+@Getter
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // OrderItems ? How to create them ? How to Link
+
+    LocalDateTime orderDate;
+    BigDecimal totalAmount;
 }
