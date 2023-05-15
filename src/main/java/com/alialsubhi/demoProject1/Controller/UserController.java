fd005a0ce045fc5b9d6921f0d0d937ed47e761f1
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 11:35:48 +0400

    UserController

--- src/main/java/com/alialsubhi/demoProject1/Controller/UserController.java
+++ src/main/java/com/alialsubhi/demoProject1/Controller/UserController.java
@@ -1,4 +1,24 @@
 package com.alialsubhi.demoProject1.Controller;
 
+import com.alialsubhi.demoProject1.Models.ShoppingCart;
+import com.alialsubhi.demoProject1.Models.User;
+import com.alialsubhi.demoProject1.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="User")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
