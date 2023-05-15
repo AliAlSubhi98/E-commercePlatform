ffaf343f27b64793f92fc87e7082f4d06834ac38
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 11:09:00 +0400

    UserService

---
+++ src/main/java/com/alialsubhi/demoProject1/Services/UserService.java
@@ -0,0 +1,19 @@
+package com.alialsubhi.demoProject1.Services;
+
+import com.alialsubhi.demoProject1.Models.User;
+import com.alialsubhi.demoProject1.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
