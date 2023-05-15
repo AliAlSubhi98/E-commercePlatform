dce6cdebb13f87fb1c28204e5efc734cca81e4c7
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 10:07:31 +0400

    create class Payment

--- src/main/java/com/alialsubhi/demoProject1/Models/Payment.java
+++ src/main/java/com/alialsubhi/demoProject1/Models/Payment.java
@@ -1,4 +1,25 @@
 package com.alialsubhi.demoProject1.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Data
+@Setter
+@Getter
+@Entity
+@Table(name = "payments")
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    // Order order How to create the link @OneToOne ?
+
+    BigDecimal amount;
+    LocalDateTime paymentDate;
 }
