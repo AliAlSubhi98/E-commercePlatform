c48ddc5eb4117bebd7ca409c8c32da7d0f4697a0
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 09:54:58 +0400

    create class Product

--- src/main/java/com/alialsubhi/demoProject1/Models/Product.java
+++ src/main/java/com/alialsubhi/demoProject1/Models/Product.java
@@ -1,4 +1,23 @@
 package com.alialsubhi.demoProject1.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+@Data
+@Setter
+@Getter
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
 }
