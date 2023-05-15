4b227577dc868d590fee4b987cfbc436c066be50
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 11:30:28 +0400

    RatingController

--- src/main/java/com/alialsubhi/demoProject1/Controller/RatingController.java
+++ src/main/java/com/alialsubhi/demoProject1/Controller/RatingController.java
@@ -1,4 +1,24 @@
 package com.alialsubhi.demoProject1.Controller;
 
+import com.alialsubhi.demoProject1.Models.Product;
+import com.alialsubhi.demoProject1.Models.Rating;
+import com.alialsubhi.demoProject1.Services.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="Rating")
 public class RatingController {
+
+    @Autowired
+    RatingService ratingService;
+
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Rating> getAllRatings() {
+        return ratingService.getAllRatings();
+    }
 }
