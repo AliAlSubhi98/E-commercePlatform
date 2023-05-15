9155892c06ae5a7ef35e8823921445a8810e6b00
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 11:04:43 +0400

    ReviewService

---
+++ src/main/java/com/alialsubhi/demoProject1/Services/ReviewService.java
@@ -0,0 +1,20 @@
+package com.alialsubhi.demoProject1.Services;
+
+import com.alialsubhi.demoProject1.Models.Review;
+import com.alialsubhi.demoProject1.Repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    List<Review> getAllReviews(){
+        return reviewRepository.findAll();
+    }
+
+}
