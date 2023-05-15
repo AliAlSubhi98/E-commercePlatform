2b9d3c2b507af0a4dc5c4f4372f4f66a14501fd6
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 10:50:39 +0400

    create the repositoreis

--- src/main/java/com/alialsubhi/demoProject1/Repositories/PaymentRepository.java
+++ src/main/java/com/alialsubhi/demoProject1/Repositories/PaymentRepository.java
@@ -1,4 +1,9 @@
 package com.alialsubhi.demoProject1.Repositories;
 
-public interface PaymentRepository {
+import com.alialsubhi.demoProject1.Models.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Long> {
 }
--- src/main/java/com/alialsubhi/demoProject1/Repositories/ReviewRepository.java
+++ src/main/java/com/alialsubhi/demoProject1/Repositories/ReviewRepository.java
@@ -1,4 +1,8 @@
 package com.alialsubhi.demoProject1.Repositories;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
+
 public interface ReviewRepository {
 }
