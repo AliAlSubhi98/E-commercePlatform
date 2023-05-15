f7858544b5766b39827de2366ba2686c781fdce8
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 11:07:51 +0400

    ShoppingCartService

---
+++ src/main/java/com/alialsubhi/demoProject1/Services/ShoppingCartService.java
@@ -0,0 +1,20 @@
+package com.alialsubhi.demoProject1.Services;
+
+import com.alialsubhi.demoProject1.Models.ShoppingCart;
+import com.alialsubhi.demoProject1.Repositories.ShoppingCartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShoppingCartService {
+
+    @Autowired
+    ShoppingCartRepository shoppingCartRepository;
+
+    List<ShoppingCart> getAllShoppingCarts(){
+
+        return shoppingCartRepository.findAll();
+    }
+}
