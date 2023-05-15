4fe08a42c0fd68d6833fbdce8109ad9a5aaa027e
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 10:28:17 +0400

    create class ShippingAddresses

--- src/main/java/com/alialsubhi/demoProject1/Models/ShippingAddresses.java
+++ src/main/java/com/alialsubhi/demoProject1/Models/ShippingAddresses.java
@@ -1,4 +1,27 @@
 package com.alialsubhi.demoProject1.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Data
+@Setter
+@Getter
+@Entity
+@Table(name = "shipping_addresses")
 public class ShippingAddresses {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    //User user @ManyToOne
+
+    String address;
+    String city;
+    String state;
+    String country;
+    String postalCode;
 }
--- src/main/java/com/alialsubhi/demoProject1/Models/ShoppingCart.java
+++ src/main/java/com/alialsubhi/demoProject1/Models/ShoppingCart.java
@@ -1,4 +1,19 @@
 package com.alialsubhi.demoProject1.Models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    // User user @OneToOne
+
+    // List<CartItem> cartItems @OneToMany
+
+
 }
