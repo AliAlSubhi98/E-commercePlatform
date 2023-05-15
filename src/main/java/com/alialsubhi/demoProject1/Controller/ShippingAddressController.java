19f2a07e97e6f31b69b3ae9b865014400908fe76
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-15 11:33:28 +0400

    ShippingAddressController

--- src/main/java/com/alialsubhi/demoProject1/Controller/ShippingAddressController.java
+++ src/main/java/com/alialsubhi/demoProject1/Controller/ShippingAddressController.java
@@ -1,4 +1,24 @@
 package com.alialsubhi.demoProject1.Controller;
 
+import com.alialsubhi.demoProject1.Models.Review;
+import com.alialsubhi.demoProject1.Models.ShippingAddress;
+import com.alialsubhi.demoProject1.Services.ShippingAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="ShippingAddress")
 public class ShippingAddressController {
+
+    @Autowired
+    ShippingAddressService shippingAddressService;
+
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<ShippingAddress> getAllShippingAddresses() {
+        return shippingAddressService.getAllShippingAddresses();
+    }
 }
